7ec64444a1e2f131073795707bc0b86155c1a68d
chriswhitmire <chriswhitmire94@gmail.com>
2022-10-02 20:57:44 -0400

    Added missing scripts back to element icons and crafting slots

--- Assets/Scripts/SpellSlot.cs
+++ Assets/Scripts/SpellSlot.cs
@@ -9,6 +9,7 @@ public class SpellSlot : MonoBehaviour, IDropHandler
 {
     
     Image image;
+    Spell spell = null;
 
 
     private void Start() {
@@ -20,6 +21,9 @@ public class SpellSlot : MonoBehaviour, IDropHandler
         Debug.Log("Dropped");
         GameObject droppedSpell = eventData.pointerDrag;
 
+        Debug.Log(this.spell);
+        spell = droppedSpell.GetComponent<SpellDisplay>().getSpell();
         image.sprite = droppedSpell.GetComponent<Image>().sprite;
+        Debug.Log(this.spell);
     }
 }
