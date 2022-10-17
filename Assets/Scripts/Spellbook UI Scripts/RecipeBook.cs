78012977a5e9d6c8ca81a1ac89fa3ca338b1d491
chriswhitmire <chriswhitmire94@gmail.com>
2022-10-16 23:11:18 -0400

    Implemented projectile, aoe, and self modifier into UI and spellcrafting
    - made aoe and self spell scriptable objects
    - edited RecipeBook script so that the recipes all expect to get a modifier

--- Assets/Scripts/Spellbook UI Scripts/RecipeBook.cs
+++ Assets/Scripts/Spellbook UI Scripts/RecipeBook.cs
@@ -8,6 +8,7 @@ public class RecipeBook : MonoBehaviour
     
     List<List<Spell>> recipeList = new List<List<Spell>>();
 
+    // Spells/Elements
     [SerializeField] Spell empty;
     [SerializeField] Spell fire;
     [SerializeField] Spell rock;
@@ -15,6 +16,12 @@ public class RecipeBook : MonoBehaviour
     [SerializeField] Spell lava;
     [SerializeField] Spell steam;
     [SerializeField] Spell mud;
+
+    // Modifiers
+    [SerializeField] Spell projectile;
+    [SerializeField] Spell aoe;
+    [SerializeField] Spell self;
+    
 
     Spell currentSpell;
     Image image;
@@ -25,9 +32,9 @@ public class RecipeBook : MonoBehaviour
 
     void setupRecipes()
     {
-        recipeList.Add(new List<Spell> {fire, rock, lava});
-        recipeList.Add(new List<Spell> {fire, water, steam});
-        recipeList.Add(new List<Spell> {water, rock, mud});
+        recipeList.Add(new List<Spell> {fire, rock, projectile, lava});
+        recipeList.Add(new List<Spell> {fire, water, aoe, steam});
+        recipeList.Add(new List<Spell> {water, rock, self, mud});
     }
                                 
         void Start()
@@ -41,16 +48,17 @@ public class RecipeBook : MonoBehaviour
         defaultSprite = image.sprite;
     }
 
-    public void getResult(Spell spell1, Spell spell2)
+    public void getResult(Spell spell1, Spell spell2, Spell modifier)
     {
 
         foreach (List<Spell> subList in recipeList)
         {
             Spell ingredient1 = subList[0];
             Spell ingredient2 = subList[1];
-            Spell result = subList[2];
+            Spell ingredient3 = subList[2];
+            Spell result = subList[3];
 
-            if ( (ingredient1 == spell1 && ingredient2 == spell2)  || (ingredient2 == spell1 && ingredient1 == spell2) )
+            if ( ((ingredient1 == spell1 && ingredient2 == spell2)  || (ingredient2 == spell1 && ingredient1 == spell2)) && (ingredient3 == modifier))
             {
                 spellDisplay.setSpell(result);
             }
