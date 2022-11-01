f4d2fa7e3b0875e8f26aaf111cc8236326ab1f3f
chriswhitmire <chriswhitmire94@gmail.com>
2022-10-31 22:55:46 -0400

    Remove unecessary attributes from spell scriptable object

--- Assets/Scripts/Scriptable Objects/Spell.cs
+++ Assets/Scripts/Scriptable Objects/Spell.cs
@@ -12,15 +12,10 @@ public class Spell : ScriptableObject {
     public int id; // thousands is type, hundreds/tens is effect it can give
 
     // 0 = null, projectile = 1, aoe = 2, self = 3
-    public int spellType = 0;
     public float cooldown;
     public float damage=0f;
     public GameObject prefab;
     public float originDistance;    //projectile = 1, aoe = var, self = 0
-
-    public bool canCauseBurning=false;
-    public bool canCauseWet=false;
-    public bool canCauseSlow=false;
 
     public float speed = 0f;
 
